b9986b7bcf587ccc438fec729ac93b530ae81e75
Niclas Collmer <niclas.collmer@9elements.com>
2022-05-28 15:05:27 +0200

    Speech synthethis

--- app/javascript/controllers/speech_controller.js
+++ app/javascript/controllers/speech_controller.js
@@ -8,15 +8,29 @@ const text = [
 const todes_noice = ("Todes Noice");
 const goarnicht_noice = ("Todes Noice");
 
+let msg = new SpeechSynthesisUtterance();
+let voices = speechSynthesis.getVoices();
+msg.voice = voices[0];
+msg.voiceURI = 'native';
+msg.volume = 1; // 0 to 1
+msg.rate = 1; // 0.1 to 10
+msg.pitch = 2; //0 to 2
+msg.lang = 'de-DE';
+
 export default class extends Controller {
+  static targets = [
+    "next_button",
+    "text"
+  ];
+
 
   speak(i) {
-    this.element.textContent = text[i]
-    window.speechSynthesis.speak(new SpeechSynthesisUtterance('Bei welcher Angelegenheit darf ich euch helfen?'));
+    this.textTarget.textContent = text[i]
+    window.speechSynthesis.speak(new SpeechSynthesisUtterance(text[i]));
   };
 
   connect() {
-    speak()
+    this.speak(1);
   };
 
 }
